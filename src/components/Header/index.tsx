1a8eaec8d571874070356219ac459a0430679055
Bruno Gomes <obrunogomes@obrunogomes.local>
2025-01-24 11:24:02 -0300

    init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Container, Logo, BackIcon, BackButton } from "./style";
+import logoImg from "../../../src/assets/Logo.png"
+
+type Props ={
+    showBackButton?: boolean
+}
+
+
+export function Header({ showBackButton = false }: Props){
+    return (
+        <Container>
+            {
+            showBackButton && 
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            }
+            <Logo source={logoImg} />
+        </Container>
+    )
+}
